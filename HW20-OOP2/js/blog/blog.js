56012db9bb27bac583cd9e5b664d9b9fbb3e05c9
SmaLLAlien <vlad_belousov1990@hotmail.com>
2019-12-30 22:38:41 +0200

    HW20-OOP2: add homework 20 Inheritance

---
+++ HW20-OOP2/js/blog/blog.js
@@ -0,0 +1,164 @@
+import {makeAddPostElement} from '../addPost.js';
+import {VideoPost, AudioPost, PicturePost, TextPost} from '../postClass.js';
+
+function main() {
+  const url = 'http://localhost:3000/api/list';
+  const urlBlog = 'js/blog/data-blog.json';
+
+  const getData = (url) => {
+    return fetch(url, {
+      method: 'GET',
+    });
+  };
+
+  const requests = [getData(url), getData(urlBlog)];
+  Promise.all(requests)
+    .then((responses) => {
+      return Promise.all(responses.map((response) => response.json()));
+    })
+    .then((responses) => {
+      const data = responses[1];
+      makeSectionBlog(data);
+
+      const posts = responses[0];
+      makeSectionPosts(posts);
+      makePostMoreButton();
+
+      const footer = document.getElementsByClassName('footer')[0];
+      const wrapper = document.getElementsByClassName('wrapper')[0];
+      wrapper.append(footer);
+      makeToHome();
+      makeAddPostElement();
+    })
+    .catch((error) => {
+      alert(`${error.message} check if you run server`);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', main);
+
+const makeElement = (elementType, elementClass) => {
+  const element = document.createElement(elementType);
+  element.classList.add(elementClass);
+  return element;
+};
+
+/** ******************************************************************************/
+// SECTION BLOG
+
+function makeSectionBlog(data) {
+  const fragmentHeader = document.createDocumentFragment();
+  const sectionBlog = makeElement('section', 'blog');
+  sectionBlog.id = 'blog';
+
+  const blogTop = makeBlogTop(data);
+  sectionBlog.append(blogTop);
+
+  const containerBlog = makeElement('div', 'container');
+  sectionBlog.append(containerBlog);
+
+  const rowBlog = makeElement('div', 'row');
+  containerBlog.insertAdjacentElement('afterbegin', rowBlog);
+
+  const blogSearch = makeInputSearch();
+  rowBlog.append(blogSearch);
+
+  fragmentHeader.append(sectionBlog);
+  const wrapper = document.getElementsByClassName('wrapper')[0];
+  wrapper.append(fragmentHeader);
+}
+
+function makeBlogTop(data) {
+  const blogTop = makeElement('div', 'blog__top');
+
+  const blogTopHeader = makeElement('h2', 'blog__header');
+  blogTop.insertAdjacentElement('afterbegin', blogTopHeader);
+  blogTopHeader.textContent = data[0].blog.header;
+
+  const blogTopFigure = makeElement('div','blog__figure');
+  blogTop.append(blogTopFigure);
+
+  return blogTop;
+}
+
+function makeInputSearch() {
+  const blogSearch = makeElement('div', 'blog__search');
+
+  const blogSearchInput = makeElement('input', 'blog__input');
+  blogSearch.append(blogSearchInput);
+  blogSearch.type = 'search';
+  blogSearchInput.placeholder = 'Search by author';
+
+  return blogSearch;
+}
+
+/** ******************************************************************************/
+// SECTION POSTS
+function makeSectionPosts(posts) {
+  const fragmentPosts = document.createDocumentFragment();
+  const sectionPosts = document.createElement('section');
+  fragmentPosts.append(sectionPosts);
+  sectionPosts.id = 'posts';
+
+  const containerPosts = makeElement('div', 'container');
+  sectionPosts.append(containerPosts);
+
+  const rowPosts = makeElement('div', 'row');
+  containerPosts.insertAdjacentElement('afterbegin', rowPosts);
+
+  const sectionPostsPosts = makeElement('div', 'posts');
+  rowPosts.append(sectionPostsPosts);
+
+  const postsOfArray = [...posts];
+
+  postsOfArray.forEach((postJson) => {
+    if (postJson.type === 'Audio') {
+      const audioPost = new AudioPost(postJson);
+      audioPost.renderBlogPost(sectionPostsPosts);
+    } else if (postJson.type === 'Video') {
+      const videoPost = new VideoPost(postJson);
+      videoPost.renderBlogPost(sectionPostsPosts);
+    } else if (postJson.type === 'Picture') {
+      const picturePost = new PicturePost(postJson);
+      picturePost.renderBlogPost(sectionPostsPosts);
+    } else {
+      const textPost = new TextPost(postJson);
+      textPost.renderBlogPost(sectionPostsPosts);
+    }
+  });
+
+  const wrapper = document.getElementsByClassName('wrapper')[0];
+  fragmentPosts.append(sectionPosts);
+  wrapper.append(fragmentPosts);
+}
+
+function makePostMoreButton() {
+  const postMoreButton = makeElement('button', 'posts__load');
+  postMoreButton.textContent = 'Read more';
+  document.getElementsByClassName('posts')[0].append(postMoreButton);
+}
+
+/** ******************************************************************************/
+// TO HOME
+
+function makeToHome() {
+  const fragmentToHome = document.createDocumentFragment();
+  const toHome = makeElement('div', 'to-home');
+
+  const toHomeLink = makeElement('a', 'to-home__link');
+  toHomeLink.href = '#home';
+
+  const toHomeTriangle = makeElement('div', 'triangle');
+  const toHomeTriangleLeft = makeElement('div', 'triangle__l');
+  const toHomeTriangleRight = makeElement('div', 'triangle__r');
+
+  toHomeTriangle.append(toHomeTriangleLeft);
+  toHomeTriangle.append(toHomeTriangleRight);
+
+  toHome.append(toHomeLink);
+  toHomeLink.append(toHomeTriangle);
+
+  fragmentToHome.append(toHome);
+  document.body.append(fragmentToHome);
+}
+
